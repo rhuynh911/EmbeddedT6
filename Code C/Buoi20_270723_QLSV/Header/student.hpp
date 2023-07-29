501b174d409c8358242f72cc6ab2bafbc6945e91
Richard Huynh <rhuynh911@users.noreply.github.com>
2023-07-29 21:57:22 +0700

    Add files via upload

--- Code C/Buoi20_270723_QLSV/Header/student.hpp
+++ Code C/Buoi20_270723_QLSV/Header/student.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 class Student {
-public:
+private:
     int id;
     std::string name;
     char gender;
@@ -15,8 +15,34 @@ public:
     double averageScore;
     std::string academicPerformance;
 
+public:
+    // Constructor
     Student(int _id, std::string _name, char _gender, int _age, double _math, double _physics, double _chemistry);
 
+    // Getter methods
+    int getId() const;
+    std::string getName() const;
+    char getGender() const;
+    int getAge() const;
+    double getMathScore() const;
+    double getPhysicsScore() const;
+    double getChemistryScore() const;
+    double getAverageScore() const;
+    std::string getAcademicPerformance() const;
+
+    // Setter methods
+    void setId(int _id);
+    void setName(std::string _name);
+    void setGender(char _gender);
+    void setAge(int _age);
+    void setMathScore(double _math);
+    void setPhysicsScore(double _physics);
+    void setChemistryScore(double _chemistry);
+    void setAverageScore(double _average);
+    void setAcademicPerformance(std::string _academicPerformance);
+
+    // Other methods
+    void calculateAverageScore();
     void calculateAcademicPerformance();
 };
 
